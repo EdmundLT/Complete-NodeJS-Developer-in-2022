c21c0a9c9df1c557fcd68f336f48e94837145104
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-06-28 00:31:50 -0400

    1

--- Section-17/GQL-EXAMPLE/server.js
+++ Section-17/GQL-EXAMPLE/server.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const path = require('path');
-const { graphqlHTTP } = require("express-graphql");
+const {ApolloServer} = require('apollo-server-express')
+const port = 3000;
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 const resolveArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
-const schema = makeExecutableSchema({
-  typeDefs: typesArray,
-  resolvers: resolveArray,
-})
 
-const app = express();
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
+async function startApolloSerer() {
+  const app=express();
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolveArray,
+  });
+  const server = new ApolloServer({
+    schema
+  });
+  await server.start();
+  server.applyMiddleware({app, path: '/graphql'})
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  app.get("/", (req, res) => res.send("Hello World!"));
+}
 
-    graphiql: true,
-  })
-);
-const port = 3000;
+startApolloSerer();
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
